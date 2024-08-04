439f370700a28b22dc9608b121916c118b5dba4e
HideInMatrix <davidmorgancreate@gmail.com>
2024-08-04 22:41:01 +0800

    feat:拖动进度条快进歌曲

--- components/share/player/MusicProcess.tsx
+++ components/share/player/MusicProcess.tsx
@@ -2,6 +2,8 @@
 
 import { Slider } from "@/components/ui/slider";
 import { useAudio } from "./AudioProvider";
+import { throttle } from "@/lib/utils";
+import { useEffect, useState } from "react";
 
 const formatTime = (seconds: number): string => {
   const minutes = Math.floor(seconds / 60);
@@ -12,17 +14,33 @@ const formatTime = (seconds: number): string => {
 };
 
 const MusicProcess = () => {
-  const { currentTime, duration } = useAudio();
+  const { currentTime, duration, audioRef } = useAudio();
+  const [process, setProcess] = useState(0);
+
+  useEffect(() => {
+    setProcess((currentTime / duration) * 100);
+  }, [currentTime, duration]);
+
+  const handleValueChange = (value: number[]) => {
+    setProcess(value[0]);
+  };
+
+  const handleValueCommit = throttle((value: number[]) => {
+    if (audioRef.current) {
+      audioRef.current.currentTime = (value[0] / 100) * duration;
+    }
+  }, 100);
 
   return (
     <div className="flex items-center">
-      <span className="w-10  grow">{formatTime(currentTime)}</span>
+      <span className="w-10 grow">{formatTime(currentTime)}</span>
       <Slider
         className="flex-auto ml-3 mr-1"
-        value={[(currentTime / duration) * 100]}
+        value={[process]}
+        onValueChange={handleValueChange}
+        onValueCommit={handleValueCommit}
       />
-
-      <span className="w-10  grow">{formatTime(duration)}</span>
+      <span className="w-10 grow">{formatTime(duration)}</span>
     </div>
   );
 };
