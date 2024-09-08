b68d97f3189b1ad75648df3a270e1b4c95e21dad
HideInMatrix <davidmorgancreate@gmail.com>
2024-09-08 17:02:02 +0800

    fix:返回提示错误

--- src/components/share/HeaderBar.tsx
+++ src/components/share/HeaderBar.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import Header from "./nav/Header";
 
 type Props = React.PropsWithChildren<{}>;
 export default function HeaderBarWrapper({ children }: Props) {
   return (
     <div className="h-full w-full flex flex-col">
-      <Header />
+      <Suspense>
+        <Header />
+      </Suspense>
       <main className="flex-auto h-full w-full gap-2 pt-12 flex flex-col">
         {children}
       </main>
