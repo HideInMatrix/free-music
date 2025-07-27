4576d338d97d7520f80b60be75d4e5cadfed2321
HideInMatrix <davidmorgancreate@gmail.com>
2025-07-27 22:34:12 +0800

    debug: react-player are in mobile

--- src/components/share/player/MusicDropAction.tsx
+++ src/components/share/player/MusicDropAction.tsx
@@ -7,7 +7,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
-import { CloudDownload, Ellipsis, ExternalLink, Files, Play, Trash } from "lucide-react";
+import { Ellipsis, ExternalLink, Files, Play, Trash } from "lucide-react";
 import { Song } from "@/entity/interface/song";
 import { useSongStore } from "@/store/useSongStore";
 import StopPropagation from "../StopPropagation";
@@ -21,7 +21,7 @@ type Props = {
 
 const MusicDropAction = ({ songInfo }: Props) => {
   const { defaultSongList } = useSongStore();
-  const { updateSongListFn, handleDownload, copyMusicName } = MusicDropActionFn(
+  const { updateSongListFn, copyMusicName } = MusicDropActionFn(
     { songInfo }
   );
   const [isShareOpen, setIsShareOpen] = useState(false);
@@ -32,48 +32,48 @@ const MusicDropAction = ({ songInfo }: Props) => {
 
   return (
     <>
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon">
-          <Ellipsis className="w-4 h-4" />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56">
-        <DropdownMenuGroup>
-          <StopPropagation>
-            <DropdownMenuItem onClick={() => updateSongListFn({ type: "add" })}>
-              <div className="flex items-center">
-                <Play strokeWidth={1} className="w-4 h-4 mr-1" />
-                播放
-              </div>
-            </DropdownMenuItem>
-          </StopPropagation>
-
-          {defaultSongList.some((song) => song.id === songInfo.id) ? (
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" size="icon">
+            <Ellipsis className="w-4 h-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuGroup>
             <StopPropagation>
-              <DropdownMenuItem
-                onClick={() => updateSongListFn({ type: "del" })}>
+              <DropdownMenuItem onClick={() => updateSongListFn({ type: "add" })}>
                 <div className="flex items-center">
-                  <Trash strokeWidth={1} className="w-4 h-4 mr-1" />
-                  删除
+                  <Play strokeWidth={1} className="w-4 h-4 mr-1" />
+                  播放
                 </div>
               </DropdownMenuItem>
             </StopPropagation>
-          ) : (
-            <></>
-          )}
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <StopPropagation>
-            <DropdownMenuItem onClick={copyMusicName}>
-              <div className="flex items-center">
-                <Files strokeWidth={1} className="w-4 h-4 mr-1" />
-                复制歌名
-              </div>
-            </DropdownMenuItem>
-          </StopPropagation>
-          {/* <StopPropagation>
+
+            {defaultSongList.some((song) => song.id === songInfo.id) ? (
+              <StopPropagation>
+                <DropdownMenuItem
+                  onClick={() => updateSongListFn({ type: "del" })}>
+                  <div className="flex items-center">
+                    <Trash strokeWidth={1} className="w-4 h-4 mr-1" />
+                    删除
+                  </div>
+                </DropdownMenuItem>
+              </StopPropagation>
+            ) : (
+              <></>
+            )}
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <StopPropagation>
+              <DropdownMenuItem onClick={copyMusicName}>
+                <div className="flex items-center">
+                  <Files strokeWidth={1} className="w-4 h-4 mr-1" />
+                  复制歌名
+                </div>
+              </DropdownMenuItem>
+            </StopPropagation>
+            {/* <StopPropagation>
             <DropdownMenuItem onClick={handleDownload}>
               <div className="flex items-center">
                 <CloudDownload strokeWidth={1} className="w-4 h-4 mr-1" />
@@ -81,24 +81,24 @@ const MusicDropAction = ({ songInfo }: Props) => {
               </div>
             </DropdownMenuItem>
           </StopPropagation> */}
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator />
-        <StopPropagation>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <StopPropagation>
             <DropdownMenuItem onClick={handleShareClick}>
               <div className="flex items-center">
                 <ExternalLink strokeWidth={1} className="w-4 h-4 mr-1" />
                 分享歌曲
               </div>
             </DropdownMenuItem>
-          </StopPropagation> 
-      </DropdownMenuContent>
-    </DropdownMenu>
-    
-    <MusicSharePanel 
-      isOpen={isShareOpen} 
-      onOpenChange={setIsShareOpen} 
-      songInfo={songInfo} 
-    />
+          </StopPropagation>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <MusicSharePanel
+        isOpen={isShareOpen}
+        onOpenChange={setIsShareOpen}
+        songInfo={songInfo}
+      />
     </>
   );
 };
